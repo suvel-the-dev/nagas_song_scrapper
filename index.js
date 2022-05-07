32aef84dc9c5d52ae772a79d616950f932141350
Suvel <suvelrathneswar08@gmail.com>
2022-05-07 18:39:30 +0530

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+
+const searchInput = 'nadaswaram tamil songs in tamil movies';
+const ExpResultCount = 50;
+let results = [];
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false }); // default is true
+    const page = await browser.newPage();
+    const searchIpParsed = searchInput.split(' ').join('+');
+
+    await page.goto(`https://www.google.com/search?q=${searchIpParsed}`);
+
+    console.log('waiting for results...');
+
+    while (results.length < ExpResultCount) {
+
+        await page.waitForSelector('input[aria-label="Search"]', { visible: true });
+        const searchResults = await page.$$eval(".LC20lb", els =>
+            els.map(e => ({ title: e.innerText, link: e.parentNode.href }))
+        );
+
+        if (searchResults?.length > 0) {
+            results = [...results, ...searchResults];
+        }
+
+        console.log(`Received ${results.length}/${ExpResultCount} results`);
+
+        const NxtPage = await page.$('a#pnnext');
+        await NxtPage.evaluate(NP => NP.click());
+    }
+
+
+    console.log('results received...');
+    console.log(results);
+
+    //   await browser.close();
+})();
